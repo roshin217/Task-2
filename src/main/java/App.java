edffbe5e61c46201442ed122b485ab7831560307
roshin217 <fry0402@gmail.com>
2024-03-12 11:53:57 +0500

    Git remote remove origin

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(helloWorld == bean);
+        System.out.println(cat1 == cat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+public class Cat {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
